7e9790bcf81fb52a19161bd7b5aec1e4c3601e1d
joel.navarro@ucb.edu.bo <joel.navarro@ucb.edu.bo>
2024-08-29 13:56:23 -0400

    Mostrar numero de items mejora

--- src/presenter.js
+++ src/presenter.js
@@ -2,12 +2,12 @@ import { calcularPrecioNeto } from "./calcularPrecio";
 
 const items = document.querySelector("#items-numero");
 const form = document.querySelector("#calcularPrecio-form");
-const div = document.querySelector("#resultado-div");
+const divnumItems = document.querySelector("#numItems-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const numItems = Number.parseInt(items.value);
 
-  div.innerHTML = "<p>" + calcularPrecioNeto(numItems) + "</p>";
+  divnumItems.innerHTML = "<p> Numero de Items: " + calcularPrecioNeto(numItems) + "</p>";
 });
